ced802d4a5223d4fc41bc251fe023ae6266ac85e
houhan@gmail.com <han.hou@alleninstitute.org>
2023-11-22 01:33:38 +0000

    simulate CurriculumManager using real data from bpod

--- code/dynamic_foraging_curriculum/automation.py
+++ code/dynamic_foraging_curriculum/automation.py
@@ -2,6 +2,7 @@
 Get data from the behavior master table and give suggestions
 """
 # %%
+import os
 import logging
 
 import pandas as pd
@@ -11,126 +12,144 @@ from dynamic_foraging_curriculum.curriculums.coupled_baiting import coupled_bait
 
 logger = logging.getLogger(__name__)
 
-# %%
+
 class CurriculumManager:
 
     def __init__(self, df_manager_path: str = None, df_master_path: str = None):
         """load df_curriculum_manager and df_master from s3"""
-        pass
+
+        # Load behavior master table
+        if df_master_path is None:
+            df_master_path = "/root/capsule/data/s3_foraging_all_nwb/df_sessions.pkl"
+        self.df_master = pd.read_pickle(df_master_path)
+
+        # Tweaks of the master table
+        self.df_master = self.df_master.sort_values(
+            by=['subject_id', 'session'], ascending=True).reset_index().iloc[:100]
+        self.task_mapper = {'coupled_block_baiting': 'Coupled Baiting'}
+
+        # Load curriculum manager table; if not exist, create a new one
+        if df_manager_path is None:
+            df_manager_path = "/root/capsule/code/dynamic_foraging_curriculum/df_curriculum_manager.csv"
+
+        if os.path.exists(df_manager_path):
+            self.df_manager = pd.read_csv(df_manager_path)
+        else:
+            self.df_manager = pd.DataFrame(columns=['subject_id', 'session_date', 'task',
+                                                    'session', 'session_at_current_stage',
+                                                    'curriculum_version', 'task_schema_version',
+                                                    'foraging_efficiency', 'finished_trials',
+                                                    'metrics', 'current_stage_suggested', 'current_stage_actual',
+                                                    'decision', 'next_stage_suggested'])
 
     def upload_df(self):
         """Upload the df_manager_path"""
 
         pass
 
+    def _count_session_at_current_stage(self,
+                                        df: pd.DataFrame,
+                                        subject_id: str,
+                                        current_stage: str) -> int:
+        """ Count the number of sessions at the current stage (reset after rolling back) """
+        session_at_current_stage = 1
+        for stage in reversed(df.query(f'subject_id == {subject_id}')['current_stage_suggested'].to_list()):
+            if stage != current_stage:
+                break
+            else:
+                session_at_current_stage += 1
+
+        return session_at_current_stage
+
+    def add_and_evaluate_session(self, subject_id, session):
+        """ Add a session to the curriculum manager and evaluate the transition """
+
+        df = self.df_manager  # Alias
+        df_this = self.df_master.query(
+            f'subject_id == {subject_id} and session == {session}').iloc[0]
+
+        # If we don't have feedback from the GUI about the actual training stage used
+        if 'actual_stage' not in self.df_master:
+            if session == 1:  # If this is the first session
+                current_stage = 'STAGE_1'
+            else:
+                # Assuming current session uses the suggested stage from the previous session
+                current_stage = self.df_manager.query(
+                    f"subject_id == {subject_id} and session == {session - 1}")[
+                        'next_stage_suggested'].values[0]
+
+        # Get metrics history
+        df_history = self.df_master.query(
+            f"subject_id == {subject_id} and session <= {session}")
+
+        performance = {
+            # Already sorted by session
+            'foraging_efficiency': df_history['foraging_eff'].to_list(),
+            'finished_trials': df_history['finished_trials'].to_list(),
+        }
+
+        # Count session_at_current_stage
+        session_at_current_stage = self._count_session_at_current_stage(
+            df, subject_id, current_stage)
+
+        # Evaluate
+        metrics = dict(**performance,
+                       session_total=session,
+                       session_at_current_stage=session_at_current_stage)
+
+        # TODO: to use the correct version of curriculum
+        decision, next_stage_suggested = coupled_baiting_curriculum.evaluate_transitions(
+            current_stage=TrainingStage[current_stage],
+            metrics=Metrics(**metrics))
+
+        # Add to the manager
+        df.loc[len(df)] = dict(
+            subject_id=subject_id,
+            session_date=df_this.session_date,
+            session=session,
+            task=self.task_mapper[df_this.task],
+            curriculum_version='0.1',  # Allows changing curriculum during training
+            task_schema_version='1.0',  # Allows changing task schema during training
+            session_at_current_stage=session_at_current_stage,
+            current_stage_suggested=current_stage,
+            foraging_efficiency=df_this.foraging_eff,
+            finished_trials=df_this.finished_trials,
+            metrics=metrics,
+            decision=decision.name,
+            next_stage_suggested=next_stage_suggested.name
+        )
+
+        self.df_manager = df
+
     def update(self):
         """update each mouse's training stage"""
+        session_key = ['subject_id', 'session']
+
         # Diff the to dataframe to find the new mice / new sessions
-        
+        df_merge = self.df_master.merge(self.df_manager, on=session_key,
+                                        how='left', indicator=True)
+        df_new_sessions_all = self.df_master[df_merge['_merge'] == 'left_only']
+        unique_subjects_to_evaluate = df_new_sessions_all['subject_id'].unique(
+        )
+        logger.info(
+            f"Found {len(df_new_sessions_all)} new sessions from {len(unique_subjects_to_evaluate)} mice to evaluate")
+
         # Loop over all mouse
-        
-            # For each session of the mouse
-            # Assuming STAGE suggestions were followed (how to get feedback from the GUI about the actual stages uesd?)
-            # Generate all decisions and next_stages
-            
-        # Upload the df_master_path
-             
-        pass
-    
+        for subject_id in unique_subjects_to_evaluate:
+            df_new_sessions = df_new_sessions_all.query(
+                f'subject_id == {subject_id}')
+            # Loop over all new sessions
+            for session in df_new_sessions['session']:
+                self.add_and_evaluate_session(subject_id, session)
+
+            pass
+
     def plot(self):
         # Plot the training history of a mouse
-        
+
         pass
-    
-
-# %%
-
-df_curriculum_manager = pd.DataFrame(columns=['subject_id', 'session_date', 'task',
-                                              'session_total', 'session_at_current_stage',
-                                              'curriculum_version', 'task_schema_version',
-                                              'foraging_efficiency', 'finished_trials',
-                                              'metrics', 'current_stage', 'decision', 'next_stage'])
-
-foraging_efficiencies = [0.5, 0.6, 0.5, 0.6, 0.6,
-                         0.6, 0.7, 0.75, 0.75, 0.8, 0.8, 0.8, 0.8]
-finished_trials = [500, 500, 500, 500, 500,
-                   500, 500, 500, 500, 200, 200, 1000, 1000]
 
 
-# TODO: define a class called CurriculumManager that interact with the database (local DataFrame or on S3)
-
-# %%
-def _count_session_at_current_stage(df: pd.DataFrame, 
-                                    subject_id: str, 
-                                    current_stage: str) -> int:
-    """ Count the number of sessions at the current stage (reset after rolling back) """
-    session_at_current_stage = 1
-    for stage in reversed(df.query(f'subject_id == "{subject_id}"')['current_stage']):
-        if stage != current_stage:
-            break
-        else:
-            session_at_current_stage += 1
-    
-    return session_at_current_stage
-
-def add_and_evaluate_session(df,
-                             session_meta: dict,  # Meta data
-                             current_stage: str,
-                             # Only performance of the new session dict(foraging_efficiency: float, finished_trials: int)
-                             current_metric: dict,
-                             ) -> pd.DataFrame:
-    """ Add a session to the curriculum manager and evaluate the transition """
-    # Count session_at_current_stage
-    session_at_current_stage = _count_session_at_current_stage(df, session_meta['subject_id'], current_stage)
-
-    # Append new performance to metrics
-    previous_metrics = df[(df['subject_id'] == session_meta['subject_id']) & (
-        df['session_total'] == session_meta['session_total'] - 1)]['metrics'].values[0] if session_meta['session_total'] > 1 else dict(foraging_efficiency=[], finished_trials=[])
-
-    metrics = dict(foraging_efficiency=previous_metrics['foraging_efficiency'] + [current_metric['foraging_efficiency']],
-                   finished_trials=previous_metrics['finished_trials'] + [
-                       current_metric['finished_trials']],
-                   )
-
-    # Evaluate
-    # TODO: to use the correct version of curriculum
-    decision, next_stage = coupled_baiting_curriculum.evaluate_transitions(current_stage=TrainingStage[current_stage],
-                                                                           metrics=Metrics(**metrics,
-                                                                                           session_total=session,
-                                                                                           session_at_current_stage=session_at_current_stage))
-
-    # Add to the manager
-    df.loc[len(df)] = dict(**session_meta,
-                           session_at_current_stage=session_at_current_stage,
-                           current_stage=current_stage,
-                           foraging_efficiency=current_metric['foraging_efficiency'],
-                           finished_trials=current_metric['finished_trials'],
-                           metrics=metrics,
-                           decision=decision.name,
-                           next_stage=next_stage.name
-                           )
-
-    return df
-
-
-# Simulate trainings
-for session, (foraging_efficiency, finished_trial) in enumerate(zip(foraging_efficiencies, finished_trials)):
-
-    df_curriculum_manager = add_and_evaluate_session(df_curriculum_manager,
-                                                     session_meta=dict(
-                                                         subject_id='test_01',
-                                                         session_date='2023-11-17',
-                                                         session_total=session + 1,
-                                                         task='Coupled Baiting',
-                                                         curriculum_version='0.1',  # Allows changing curriculum during training
-                                                         task_schema_version='1.0',  # Allows changing task schema during training
-                                                     ),
-                                                     current_stage='STAGE_1'
-                                                                    if session == 0
-                                                                    else df_curriculum_manager[df_curriculum_manager['session_total'] == session]['next_stage'].values[0],
-                                                     current_metric={'foraging_efficiency': foraging_efficiency,
-                                                                     'finished_trials': finished_trial,
-                                                                     }
-                                                     )
-
-print(df_curriculum_manager)
+manager = CurriculumManager()
+manager.df_manager
+manager.update()
