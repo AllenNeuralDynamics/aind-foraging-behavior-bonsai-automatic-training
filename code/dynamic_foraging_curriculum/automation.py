a7bfa751602623c9a3d679d282398eaa2f89e9f0
houhan@gmail.com <han.hou@alleninstitute.org>
2023-11-28 08:13:12 +0000

    add plot_all_progress

--- code/dynamic_foraging_curriculum/automation.py
+++ code/dynamic_foraging_curriculum/automation.py
@@ -5,7 +5,9 @@ Get data from the behavior master table and give suggestions
 import os
 import logging
 
+import numpy as np
 import pandas as pd
+import plotly.graph_objects as go
 
 from dynamic_foraging_curriculum.schema.curriculum import TrainingStage, Metrics
 from dynamic_foraging_curriculum.curriculums.coupled_baiting import coupled_baiting_curriculum
@@ -24,8 +26,9 @@ class CurriculumManager:
         self.df_master = pd.read_pickle(df_master_path)
 
         # Tweaks of the master table
-        self.df_master = self.df_master.sort_values(
-            by=['subject_id', 'session'], ascending=True).reset_index().iloc[:100]
+        self.df_master = self.df_master.query(
+            "task == 'coupled_block_baiting'").sort_values(
+            by=['subject_id', 'session'], ascending=True).reset_index()
         self.task_mapper = {'coupled_block_baiting': 'Coupled Baiting'}
 
         # Load curriculum manager table; if not exist, create a new one
@@ -64,8 +67,7 @@ class CurriculumManager:
     def add_and_evaluate_session(self, subject_id, session):
         """ Add a session to the curriculum manager and evaluate the transition """
 
-        df_this = self.df_master.query(
-            f'subject_id == {subject_id} and session == {session}').iloc[0]
+        df_this_mouse = self.df_manager.query(f'subject_id == {subject_id}')
 
         # If we don't have feedback from the GUI about the actual training stage used
         if 'actual_stage' not in self.df_master:
@@ -73,9 +75,24 @@ class CurriculumManager:
                 current_stage = 'STAGE_1'
             else:
                 # Assuming current session uses the suggested stage from the previous session
-                current_stage = self.df_manager.query(
-                    f"subject_id == {subject_id} and session == {session - 1}")[
-                        'next_stage_suggested'].values[0]
+                q_current_stage = df_this_mouse.query(
+                    f"session == {session - 1}")['next_stage_suggested']
+
+                if len(q_current_stage) > 0:
+                    current_stage = q_current_stage.iloc[0]
+                else:  # Catch missing session or wrong session number
+                    id_last_session = df_this_mouse[df_this_mouse.session < session]
+                    if len(id_last_session) > 0:
+                        id_last_session = id_last_session.session.idxmax()
+                        current_stage = df_this_mouse.loc[id_last_session,
+                                                          'next_stage_suggested']
+                        logger.warning(
+                            msg=f"Cannot find subject {subject_id} session {session - 1}, "
+                                f"use session {df_this_mouse.loc[id_last_session].session} instead")
+                    else:
+                        logger.error(
+                            msg=f"Cannot find subject {subject_id} anysession < {session}")
+                        return
 
         # Get metrics history
         df_history = self.df_master.query(
@@ -100,13 +117,10 @@ class CurriculumManager:
         decision, next_stage_suggested = coupled_baiting_curriculum.evaluate_transitions(
             current_stage=TrainingStage[current_stage],
             metrics=Metrics(**metrics))
-        
-        # Logging
-        logger.info(f"{subject_id}, {df_this.session_date}, session {session}: " +
-                    (f"STAY at {current_stage}" if decision.name == 'STAY' 
-                    else f"{decision.name} {current_stage} --> {next_stage_suggested.name}"))
 
         # Add to the manager
+        df_this = self.df_master.query(
+            f'subject_id == {subject_id} and session == {session}').iloc[0]
         self.df_manager.loc[len(self.df_manager)] = dict(
             subject_id=subject_id,
             session_date=df_this.session_date,
@@ -122,6 +136,11 @@ class CurriculumManager:
             decision=decision.name,
             next_stage_suggested=next_stage_suggested.name
         )
+
+        # Logging
+        logger.info(f"{subject_id}, {df_this.session_date}, session {session}: " +
+                    (f"STAY at {current_stage}" if decision.name == 'STAY'
+                     else f"{decision.name} {current_stage} --> {next_stage_suggested.name}"))
 
     def update(self):
         """update each mouse's training stage"""
@@ -146,15 +165,74 @@ class CurriculumManager:
 
             pass
 
-    def plot(self):
+    def plot_all_progress(self, if_show_fig=True):
         # Plot the training history of a mouse
+        df_manager = self.df_manager
 
-        pass
+        # Define color scale - mapping stages to colors from red to green
+        stage_to_numeric = {
+            TrainingStage.STAGE_1.name: 'red',
+            TrainingStage.STAGE_2.name: 'orange',
+            TrainingStage.STAGE_3.name: 'yellow',
+            TrainingStage.STAGE_FINAL.name: 'lightgreen',
+            TrainingStage.GRADUATED.name: 'green'
+        }
+
+        # Preparing the scatter plot
+        traces = []
+        for n, subject_id in enumerate(df_manager['subject_id'].unique()):
+            df_subject = df_manager[df_manager['subject_id'] == subject_id]
+            # Get h2o if available
+            if 'h2o' in self.df_master:
+                h2o = self.df_master[
+                    self.df_master['subject_id'] == subject_id]['h2o'].iloc[0]
+            else:
+                h2o = None
+
+            trace = go.Scattergl(
+                x=df_subject['session'],
+                y=[n] * len(df_subject),
+                mode='markers',
+                marker=dict(
+                    size=10,
+                    line=dict(width=1, color='black'),
+                    color=df_subject['current_stage_suggested'].map(
+                        stage_to_numeric),
+                    # colorbar=dict(title='Training Stage'),
+                ),
+                name=f'Mouse {subject_id}',
+                hovertemplate=(f"<b>Subject {subject_id} ({h2o})"
+                               "<br>Session %{x}"
+                               "<br>%{customdata[0]}</b>"
+                               "<br>foraging_eff = %{customdata[1]}"
+                               "<br>finished_trials = %{customdata[2]}"
+                               "<extra></extra>"),
+                customdata=np.stack(
+                    (df_subject.current_stage_suggested,
+                     df_subject.foraging_efficiency,
+                     df_subject.finished_trials), axis=-1),
+                showlegend=False
+            )
+            traces.append(trace)
+
+        # Create the figur  e
+        fig = go.Figure(data=traces)
+        fig.update_layout(
+            title='Training Progress of Mice',
+            xaxis_title='Session',
+            yaxis_title='Mouse'
+        )
+
+        # Show the plot
+        if if_show_fig:
+            fig.show()
+        return fig
 
 
 if __name__ == "__main__":
     manager = CurriculumManager()
     manager.df_manager
     manager.update()
+    manager.plot_all_progress()
 
 # %%
