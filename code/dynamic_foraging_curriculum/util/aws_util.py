534123b445077644ef5b87e859c82a366333bf71
houhan@gmail.com <han.hou@alleninstitute.org>
2023-12-02 00:34:33 +0000

    add download from s3

--- code/dynamic_foraging_curriculum/util/aws_util.py
+++ code/dynamic_foraging_curriculum/util/aws_util.py
@@ -8,28 +8,28 @@ BUCKET = 'aind-behavior-data'
 
 logger = logging.getLogger(__name__)
 
+s3_client = boto3.client('s3')
+
+
 def export_df_and_upload(df,
                          file_name,
                          local_cache_path='/root/capsule/results/',
                          s3_path='foraging_auto_training/',
-                         method='df_to_csv'):
+                         method='csv'):
 
     # save to local cache
     local_file_name = local_cache_path + file_name
 
-    if method == 'df_to_pickle':
+    if method == 'pickle':
         df.to_pickle(local_file_name)
-    elif method == 'df_to_csv':
+    elif method == 'csv':
         df.to_csv(local_file_name)
-    elif method == 'dill_dump':
-        dill.dump(df, open(local_file_name, 'wb'))
 
     size = os.path.getsize(local_file_name) / (1024 * 1024)
 
     # upload to s3
     s3_file_name = s3_path + file_name
     try:
-        s3_client = boto3.client('s3')
         s3_client.upload_file(local_file_name,
                               BUCKET,
                               s3_file_name)
@@ -37,6 +37,35 @@ def export_df_and_upload(df,
                     f'size = {size} MB, df_length = {len(df)}')
     except ClientError as e:
         logger.error(e)
+
+
+def download_and_load_df(file_name,
+                         local_cache_path='/root/capsule/results/',
+                         s3_path='foraging_auto_training/',
+                         method='csv'):
+
+    # download from s3
+    s3_file_name = s3_path + file_name
+    local_file_name = local_cache_path + file_name
+
+    try:
+        s3_client.download_file(BUCKET,
+                                s3_file_name,
+                                local_file_name)
+        
+        size = os.path.getsize(local_file_name) / (1024 * 1024)
+        logger.info(f'file downloaded to {BUCKET}/{s3_file_name}, '
+                    f'size = {size} MB, df_length = {len(df)}')
+    except FileNotFoundError as e:
+        logger.info(f'file not found: {e}')
+        return e
+    except ClientError as e:
+        logger.error(e)
+
+    if method == 'pickle':
+        return pd.read_pickle(local_file_name)
+    elif method == 'csv':
+        return pd.read_csv(local_file_name)
 
 
 if __name__ == '__main__':
@@ -48,4 +77,13 @@ if __name__ == '__main__':
                       'a', 'b', 'c', 'd', 'e'])
     export_df_and_upload(df,
                          file_name='test.csv',
-                         method='df_to_csv')
+                         local_cache_path='/root/capsule/results/',
+                         s3_path='foraging_auto_training/',
+                         method='csv')
+
+    df = download_and_load_df(file_name='test.csv',
+                              local_cache_path='/root/capsule/results/',
+                              s3_path='foraging_auto_training/',
+                              method='csv')
+
+    print(df)
