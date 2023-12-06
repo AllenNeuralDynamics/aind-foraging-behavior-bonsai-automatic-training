480b0303b7ca9d6cdfd55a586ab8776c685246a4
houhan@gmail.com <han.hou@alleninstitute.org>
2023-12-06 08:05:15 +0000

    logging setting

--- code/dynamic_foraging_curriculum/__init__.py
+++ code/dynamic_foraging_curriculum/__init__.py
@@ -1,1 +1,9 @@
 __version__ = "0.1"
+
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    # format="%(asctime)s - %(levelname)s - %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S",
+                    handlers=[logging.FileHandler("debug_curriculum_manager.log"), logging.StreamHandler()]
+                    )
