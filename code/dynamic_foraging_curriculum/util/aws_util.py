503c0a3740edc771dd317c8afd9aad90f1de7a8e
houhan@gmail.com <han.hou@alleninstitute.org>
2023-12-06 08:04:59 +0000

    retrieve aws credential from ~/.aws/credentials

--- code/dynamic_foraging_curriculum/util/aws_util.py
+++ code/dynamic_foraging_curriculum/util/aws_util.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import configparser
 
 import pandas as pd
 import boto3
@@ -8,7 +9,53 @@ from botocore.exceptions import ClientError
 
 logger = logging.getLogger(__name__)
 
-s3_client = boto3.client('s3')
+def get_aws_credentials(profile='default'):
+    """
+    Retrieve AWS credentials from the ~/.aws/credentials file.
+    For windows, the file should be at %UserProfile%\.aws\credentials.
+    
+    The file should look like this:
+    
+    [default]
+    aws_access_key_id=foo
+    aws_secret_access_key=bar
+    
+    See https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html
+
+    :param profile: The profile to read from the credentials file. Default is 'default'.
+    :return: A dictionary containing 'aws_access_key_id' and 'aws_secret_access_key', or None if not found.
+    """
+    # Construct the path to the credentials file
+    credentials_path = os.path.expanduser("~/.aws/credentials")
+
+    # Check if credentials file exists
+    if not os.path.exists(credentials_path):
+        logger.error("AWS credentials file not found.")
+        return None
+
+    # Read the credentials file
+    config = configparser.ConfigParser()
+    config.read(credentials_path)
+
+    # Retrieve credentials for the specified profile
+    if profile in config:
+        aws_access_key_id = config[profile].get('aws_access_key_id')
+        aws_secret_access_key = config[profile].get('aws_secret_access_key')
+        logger.info(f'Found AWS credential from ~/.aws/credentials!')
+        return {
+            'aws_access_key_id': aws_access_key_id,
+            'aws_secret_access_key': aws_secret_access_key
+        }
+    else:
+        logger.error(f"Profile '{profile}' not found in credentials file.")
+        return None
+
+
+aws_credentials = get_aws_credentials()
+s3_client = boto3.client('s3',
+                         aws_access_key_id=aws_credentials['aws_access_key_id'],
+                         aws_secret_access_key=aws_credentials['aws_secret_access_key']
+                         )
 
 
 def export_and_upload_df(df,
@@ -19,6 +66,8 @@ def export_and_upload_df(df,
 
     # save to local cache
     local_file_name = local_cache_path + file_name
+    
+    os.makedirs(local_cache_path, exist_ok=True)
 
     if file_name.split('.')[-1] == 'pkl':
         df.to_pickle(local_file_name)
@@ -48,6 +97,8 @@ def download_and_import_df(file_name,
     # download from s3
     s3_file_name = s3_path + file_name
     local_file_name = local_cache_path + file_name
+    
+    os.makedirs(local_cache_path, exist_ok=True)
 
     try:
         s3_client.download_file(bucket,
@@ -79,11 +130,11 @@ if __name__ == '__main__':
                          file_name='test.csv',
                          local_cache_path='/root/capsule/results/',
                          s3_path='foraging_auto_training/',
-                         method='csv')
+                         )
 
     df = download_and_import_df(file_name='test.csv',
                                 local_cache_path='/root/capsule/results/',
                                 s3_path='foraging_auto_training/',
-                                method='csv')
+                                )
 
     print(df)
