55e0a26bf03dd74f13e31a6be75023c60b6b59af
houhan@gmail.com <han.hou@alleninstitute.org>
2023-12-15 10:38:18 +0000

    decrease logging level to warning

--- code/aind_auto_train/__init__.py
+++ code/aind_auto_train/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.2.0"
 
 import logging
 
-logging.basicConfig(level=logging.INFO,
+logging.basicConfig(level=logging.WARNING,
                     format='%(asctime)s | %(levelname)s | %(name)s | %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     handlers=[logging.FileHandler('debug_auto_train_manager.log'),
