dda7ae9db11a07aec759510edb237e96ac106f21
houhan@gmail.com <han.hou@alleninstitute.org>
2024-01-27 20:00:34 +0000

    setup run.py to update databases

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,7 +1,37 @@
 """ top level run script """
+import threading
+import time
+
+from aind_auto_train import setup_logging
+from aind_auto_train.auto_train_manager import DynamicForagingAutoTrainManager
+setup_logging()
+
+
+def update_auto_train_database(managers):
+    while True:
+        for manager_name, manager in managers.items():
+            manager.update()
+        time.sleep(10)
 
 def run():
-    """ basic run function """
-    pass
+    # Connect to databases
+    managers = {}
+    managers['447_demo'] = DynamicForagingAutoTrainManager(
+        manager_name='447_demo',
+        df_behavior_on_s3=dict(bucket='aind-behavior-data',
+                               root='foraging_nwb_bonsai_processed/',
+                               file_name='df_sessions.pkl'),
+        df_manager_root_on_s3=dict(bucket='aind-behavior-data',
+                                   root='foraging_auto_training/'),
+        if_rerun_all=False
+    )
 
-if __name__ == "__main__": run()
+    # create a thread, and run update_auto_train_database() per 1 hour
+    update_auto_train_database(managers)
+    thread = threading.Thread(update_auto_train_database, args=[managers])
+    thread.start()
+
+
+
+if __name__ == "__main__":
+    run()
